6f7e077f6c87a6fe96f1474c0c2d2bdbb8e14378
denis <106389581+fishman123456@users.noreply.github.com>
2023-02-17 19:54:56 +0700

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace table_sql_data_17_03_2023
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
